2e8f174f9a14ca438c5c9984cc842fbcb43ff606
Edgardo Pereyra <edgardodth@gmail.com>
2018-02-15 16:52:46 +0000

    Creando plantillas de apoyo

---
+++ plantilla/navbar.inc.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Este boton despliega la barra de navegacion</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Inicio</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> Entradas</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Favoritos</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-education" aria-hidden="true"></span> Autores</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#">
+                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                    </a>
+                </li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-in" aria-hidden="true"></span> Iniciar sesion</a></li>
+                <li><a href="registro.php"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Registro</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ plantilla/registro_vacio.inc.php
@@ -0,0 +1,97 @@
+<?php
+$titulo = "Blog de tareas";
+Conexion::abrirConexion();
+$conexion = Conexion::obtenerConexion();
+$sql = 'SELECT * FROM Centro';
+if(isset($conexion)) {
+    try {
+        $sqlQuery = $conexion->prepare($sql);
+        $sqlQuery->execute();
+        $resultado = $sqlQuery->fetchAll();
+
+        $sql = 'SELECT * FROM Area';
+        $sqlQuery = $conexion->prepare($sql);
+        $sqlQuery->execute();
+        $area = $sqlQuery->fetchAll();
+    } catch (PDOException $ex) {
+        print "ERROR: ".$ex->getMessage();
+    }
+    Conexion::cerrarConexion();
+
+} else {
+    echo "fallo conexion";
+}
+
+?>
+<div class="form-group">
+    <label for="nombre">Nombre del medico</label>
+    <input type="text" name="nombre" value=""
+        placeholder="Ingresa el nombre del medico"
+        class="form-control">
+</div>
+<div class="form-group">
+    <label for="nombre">Apellido Paterno del medico</label>
+    <input type="text" name="nombre2" id="nombre" value=""
+        placeholder="Ingresa el nombre del medico"
+        class="form-control">
+</div>
+<div class="form-group">
+    <label for="nombre">Apellido Materno del medico</label>
+    <input type="text" name="nombre3" id="nombre" value=""
+        placeholder="Ingresa el nombre del medico"
+        class="form-control">
+</div>
+<div class="form-group">
+    <label for="numero-cedula">Numero de cedula</label>
+    <input type="number" name="numero-cedula"
+        id="numero-cedula" value=""
+        placeholder="Ingresa el numero de cedula"
+        class="form-control">
+</div>
+<div class="form-group">
+    <label for="centro-de-salud">
+        Centro de Salud 2
+    </label>
+    <select class="form-control" name="centro-de-salud">
+        <?php
+        $i =1;
+        foreach ($resultado as $row) {
+            extract($row);
+            echo "<OPTION VALUE=".(string)$i.">".$row['nombreCen']."\n</OPTION> ";
+            $i++;
+        }
+         ?>
+    </select>
+</div>
+<div class="form-group">
+    <label for="area-trabajo">
+        Area de Trabajo
+    </label>
+    <select class="form-control" name="area-trabajo">
+        <?php
+        $i =1;
+        foreach ($area as $row) {
+            extract($row);
+            echo "<OPTION VALUE=".(string)$i.">".$row['nombreAre']."\n</OPTION>";
+            $i++;
+        }
+        ?>
+    </select>
+</div>
+<div class="form-group">
+    <label for="pass">Contrasena</label>
+    <input type="password" name="pass" id="pass" value=""
+        placeholder="Crear un password"
+        class="form-control">
+</div>
+<div class="form-group">
+    <label for="pass">Repita su contrasena</label>
+    <input type="password" name="pass2" id="pass2" value=""
+        placeholder="Crear un password"
+        class="form-control">
+    <button type="submit" name="enviar" id="guardar"
+    class="btn btn-default btn-primary"> Guardar</button>
+
+    <a href="index.php" class="btn btn-danger btn-default"
+    role="button" aria-pressed="true">Cancelar</a>
+</div>
