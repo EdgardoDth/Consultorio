edeb104610501bad35a011502f4774d450a05ae8
Edgardo Pereyra <edgardodth@gmail.com>
2018-02-15 16:02:28 +0000

    creando vistas auxiliares

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 include_once 'app/Conexion.inc.php';
-include_once 'app/RepositorioUsuario.inc.php';
 $titulo = "Centro de Salud";
 include_once 'plantilla/documento_declaracion.inc.php';
 include_once 'plantilla/navbar.inc.php';
